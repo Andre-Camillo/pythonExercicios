adcb35f29c2ad0884cd972c5ace9d2b97e50fb75
ANDRE DOUGLAS CAMILLO <127610857+Andre-Camillo@users.noreply.github.com>
2023-03-12 07:34:27 -0300

    MELHOREI O JOKENPO

--- ex045.py
+++ ex045.py
@@ -7,7 +7,7 @@ vc = int(input('''*** OPÇÕES DE ESCOLHA ***
 \033[1;35m[ 2 ] PAPEL\033[m
 \033[1;36m[ 3 ] TESOURA\033[m
 ESCOLHA UMA DAS OPÇÕES: '''))
-sleep(0.5)
+#sleep(0.5)
 print('JO')
 sleep(0.5)
 print('KEN')
@@ -17,25 +17,25 @@ if comp == 1 and vc == 1:
     print('COMPUTADOR ESCOLHEU: \033[1;37mPEDRA\033[m\nVOCÊ ESCOLHEU: \033[1;37mPEDRA\033[m\n\033[1;33mEMPATOU{}\033[m'
           .format(emojize(':neutral_face:')))
 elif comp == 1 and vc == 2:
-    print('COMPUTADOR ESCOLHEU: \033[1;37mPEDRA\033[m\nVOCÊ ESCOLHEU: \033[1;35mPAPEL\033[m\n\033[1;32mVOCÊ VENCEU{}\033[m'
+    print('COMPUTADOR ESCOLHEU: \033[1;37mPEDRA\033[m\nVOCÊ ESCOLHEU: \033[1;35mPAPEL\033[m\n\033[1;32m"PAPEL EMBRULHA PEDRA"\nVOCÊ VENCEU{}\033[m'
           .format(emojize(':grinning_face_with_big_eyes:')))
 elif comp == 1 and vc == 3:
-    print('COMPUTADOR ESCOLHEU: \033[1;37mPEDRA\033[m\nVOCÊ ESCOLHEU: \033[1;36mTESOURA\033[m\n\033[1;31mVOCÊ PERDEU{}\033[m'
+    print('COMPUTADOR ESCOLHEU: \033[1;37mPEDRA\033[m\nVOCÊ ESCOLHEU: \033[1;36mTESOURA\033[m\n\033[1;31m"TESOURA NÃO CORTA PEDRA"\nVOCÊ PERDEU{}\033[m'
           .format(emojize(':pleading_face:')))
 elif comp == 2 and vc == 1:
-    print('COMPUTADOR ESCOLHEU: \033[1;35mPAPEL\033[m\nVOCÊ ESCOLHEU: \033[1;37mPEDRA\033[m\n\033[1;31mVOCÊ PERDEU{}\033[m'
+    print('COMPUTADOR ESCOLHEU: \033[1;35mPAPEL\033[m\nVOCÊ ESCOLHEU: \033[1;37mPEDRA\033[m\n\033[1;31m"PEDRA É EMBRULHADA POR PAPEL"\nVOCÊ PERDEU{}\033[m'
           .format(emojize(':pleading_face:')))
 elif comp == 2 and vc == 2:
     print('COMPUTADOR ESCOLHEU: \033[1;35mPAPEL\033[m\nVOCÊ ESCOLHEU: \033[1;35mPAPEL\033[m\n\033[1;33mEMPATOU{}\033[m'
           .format(emojize(':neutral_face:')))
 elif comp == 2 and vc == 3:
-    print('COMPUTADOR ESCOLHEU: \033[1;35mPAPEL\033[m\nVOCÊ ESCOLHEU: \033[1;36mTESOURA\033[m\n\033[1;32mVOCÊ VENCEU{}\033[m'
+    print('COMPUTADOR ESCOLHEU: \033[1;35mPAPEL\033[m\nVOCÊ ESCOLHEU: \033[1;36mTESOURA\033[m\n\033[1;32m"TESOURA CORTA PAPEL"\nVOCÊ VENCEU{}\033[m'
           .format(emojize(':grinning_face_with_big_eyes:')))
 elif comp == 3 and vc == 1:
-    print('COMPUTADOR ESCOLHEU: \033[1;36mTESOURA\033[m\nVOCÊ ESCOLHEU: \033[1;37mPEDRA\033[m\n\033[1;32mVOCÊ VENCEU{}\033[m'
+    print('COMPUTADOR ESCOLHEU: \033[1;36mTESOURA\033[m\nVOCÊ ESCOLHEU: \033[1;37mPEDRA\033[m\n\033[1;32m"PEDRA QUEBRA TESOURA"\nVOCÊ VENCEU{}\033[m'
           .format(emojize(':grinning_face_with_big_eyes:')))
 elif comp == 3 and vc == 2:
-    print('COMPUTADOR ESCOLHEU: \033[1;36mTESOURA\033[m\nVOCÊ ESCOLHEU: \033[1;35mPAPEL\033[m\n\033[1;31mVOCÊ PERDEU{}\033[m'
+    print('COMPUTADOR ESCOLHEU: \033[1;36mTESOURA\033[m\nVOCÊ ESCOLHEU: \033[1;35mPAPEL\033[m\n\033[1;31m"PAPEL É CORTADO PELA TESOURA"\nVOCÊ PERDEU{}\033[m'
           .format(emojize(':pleading_face:')))
 elif comp == 3 and vc == 3:
     print('COMPUTADOR ESCOLHEU: \033[1;36mTESOURA\033[m\nVOCÊ ESCOLHEU: \033[1;36mTESOURA\033[m\n\033[1;33mEMPATOU{}\033[m'
