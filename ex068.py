ff4a21e5463e8054e31f0cdd7e350f3fe831a158
ANDRE DOUGLAS CAMILLO <127610857+Andre-Camillo@users.noreply.github.com>
2023-03-28 11:52:10 -0300

    at

--- ex068.py
+++ ex068.py
@@ -1,31 +1,40 @@
 from random import randint
-print('===== PAR OU IMPAR =====')
-win = lose = 0
-escolha = str(input('======== OPÇÕES ========\nESCOLHA "PAR" ou "IMPAR" [P] ou [I]": ')).strip().upper()
-while escolha not in 'PpIi':
-    escolha = str(input('VOCÊ DEVE ESCOLHER "PAR" ou "IMPAR" [P] ou [I]": ')).strip().upper()
-comput = randint(0, 9)
-user = int(input('DIGITE UM NÚMERO: '))
-result = comput + user
-print(f'Computador escolheu: {comput}.\nVocê escolheu: {user}.\nTotal: {result}.')
-
-if result % 2 == 1:
-    clue = 'impar'
-    print(f'{result} é IMPAR.')
-else:
-    clue = 'par'
-    print(f'{result} é PAR.')
-
-if 'P' in escolha and clue == 'par':
-    win += 1
-    print('Parabéns!! Você venceu.')
-elif 'P' in escolha and clue == 'impar':
-    lose += 1
-    print('Tente novamente.')
-elif 'I' in escolha and clue == 'impar':
-    win += 1
-    print('Parabéns!! Você venceu.')
-elif 'I' in escolha and clue == 'par':
-    lose += 1
-    print('Tente novamente.')
-print(f'Vitória(s): {win}\nDerrota(s): {lose}')
+from emoji import emojize
+color = {"clear": "\033[m", "red": "\033[1;31m", "green": "\033[1;32m", "cyan": "\033[1;36m", "purple": "\033[1;35m", "<B>": "\033[1m"}
+print('========== PAR OU IMPAR ==========\n')
+nome = str(input('INFORME SEU NOME: ')).strip().upper()
+win = 0
+while True:
+    comput = randint(0, 0)
+    escolha = str(input('ESCOLHA "PAR" ou "IMPAR" [P] ou [I]": ')).strip()
+    while escolha not in 'PpIi':
+        escolha = str(input('VOCÊ DEVE ESCOLHER "PAR" ou "IMPAR" [P] ou [I]": ')).strip()
+    user = int(input('DIGITE UM NÚMERO: '))
+    result = comput + user
+    print(f'\nComputador escolheu: {comput}.\nVocê escolheu: {user}.\nTotal: {result}.\n')
+    if result % 2 == 1:
+        aposta = 'impar'
+        print(f'{result} é IMPAR.')
+    else:
+        aposta = 'par'
+        print(f'{result} é PAR.')
+    if escolha in 'Pp' and aposta == 'par':
+        win += 1
+        print(f'{color["cyan"]}CONGRATULATIONS!!{color["clear"]} {color["<B>"]}{nome}{color["clear"]}.\n{color["green"]}YOU WIN{color["clear"]}{emojize(":thumbs_up:")}')
+        print('=' * 35)
+        print('')
+    elif escolha in 'Pp' and aposta == 'impar':
+        print(f'{color["purple"]}SORRY{color["clear"]} {color["<B>"]}{nome}{color["clear"]}.'
+              f'\n{color["red"]}YOU LOOSE{color["clear"]}{emojize(":thumbs_down:")}')
+        break
+    elif escolha in 'Ii' and aposta == 'impar':
+        win += 1
+        print(f'{color["cyan"]}CONGRATULATIONS!!{color["clear"]} {color["<B>"]}{nome}{color["clear"]}.\n{color["green"]}YOU WIN{color["clear"]}{emojize(":thumbs_up:")}')
+        print('=' * 35)
+        print('')
+    elif escolha in 'Ii' and aposta == 'par':
+        print(f'{color["purple"]}SORRY{color["clear"]} {color["<B>"]}{nome}{color["clear"]}.'
+              f'\n{color["red"]}YOU LOOSE{color["clear"]}{emojize(":thumbs_down:")}')
+        break
+print(f'Total de: {win} Vitória(s).')
+print('=' * 35)
--- teste.py
+++ teste.py
@@ -1,7 +1,40 @@
-soma = cont = num = 0
-while num != 999:
-    soma += num
-    cont += 1
-    num = int(input('Digite um valor: '))
-
-print(f'Ao todo foram digitados {cont}, totalizando {soma}.')
+from random import randint
+from emoji import emojize
+color = {"clear": "\033[m", "red": "\033[1;31m", "green": "\033[1;32m", "cyan": "\033[1;36m", "purple": "\033[1;35m", "<B>": "\033[1m"}
+print('========== PAR OU IMPAR ==========\n')
+nome = str(input('INFORME SEU NOME: ')).strip().upper()
+win = 0
+while True:
+    comput = randint(0, 0)
+    escolha = str(input('ESCOLHA "PAR" ou "IMPAR" [P] ou [I]": ')).strip()
+    while escolha not in 'PpIi':
+        escolha = str(input('VOCÊ DEVE ESCOLHER "PAR" ou "IMPAR" [P] ou [I]": ')).strip()
+    user = int(input('DIGITE UM NÚMERO: '))
+    result = comput + user
+    print(f'\nComputador escolheu: {comput}.\nVocê escolheu: {user}.\nTotal: {result}.\n')
+    if result % 2 == 1:
+        aposta = 'impar'
+        print(f'{result} é IMPAR.')
+    else:
+        aposta = 'par'
+        print(f'{result} é PAR.')
+    if escolha in 'Pp' and aposta == 'par':
+        win += 1
+        print(f'{color["cyan"]}CONGRATULATIONS!!{color["clear"]} {color["<B>"]}{nome}{color["clear"]}.\n{color["green"]}YOU WIN{color["clear"]}{emojize(":thumbs_up:")}')
+        print('=' * 35)
+        print('')
+    elif escolha in 'Pp' and aposta == 'impar':
+        print(f'{color["purple"]}SORRY{color["clear"]} {color["<B>"]}{nome}{color["clear"]}.'
+              f'\n{color["red"]}YOU LOOSE{color["clear"]}{emojize(":thumbs_down:")}')
+        break
+    elif escolha in 'Ii' and aposta == 'impar':
+        win += 1
+        print(f'{color["cyan"]}CONGRATULATIONS!!{color["clear"]} {color["<B>"]}{nome}{color["clear"]}.\n{color["green"]}YOU WIN{color["clear"]}{emojize(":thumbs_up:")}')
+        print('=' * 35)
+        print('')
+    elif escolha in 'Ii' and aposta == 'par':
+        print(f'{color["purple"]}SORRY{color["clear"]} {color["<B>"]}{nome}{color["clear"]}.'
+              f'\n{color["red"]}YOU LOOSE{color["clear"]}{emojize(":thumbs_down:")}')
+        break
+print(f'Total de: {win} Vitória(s).')
+print('=' * 35)
