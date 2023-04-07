a3a3297053bd5bfec610f9b7c5e022ba981d1a3b
ANDRE DOUGLAS CAMILLO <127610857+Andre-Camillo@users.noreply.github.com>
2023-04-07 13:39:22 -0300

    Update ex069.py

--- ex069.py
+++ ex069.py
@@ -1,22 +1,20 @@
 from datetime import date
-op = str(input('========== OPÇÕES ==========\nDIGITE [ 1 ] PARA PROSSEGUIR\nDIGITE [ 0 ] PARA SAIR\nESCOLHA SUA OPÇÃO: '))
+op = - 1
+ano_atual = date.today().year
 while op != 0:
+    op = int(input(
+        '========== OPÇÕES ==========\nDIGITE [ 1 ] PARA PROSSEGUIR\nDIGITE [ 0 ] PARA SAIR\nESCOLHA SUA OPÇÃO: '))
     ano_nasc = int(input('INFORME O ANO DE SEU NASCIMENTO [XXXX]: '))
-    sexo = str(input('INFORME O SEU SEXO [M] ou [F]: '))
-
-    ano_atual = date.today().year
     idade = ano_atual - ano_nasc
-
+    sexo = str(input('INFORME O SEU SEXO [M] ou [F]: '))
     if sexo in 'Mm':
         print(f'Tens {idade} ano(s) e é do sexo Masculino.')
     elif sexo in 'Ff':
         print(f'Tens {idade} ano(s) e é do sexo Feminino.')
     else:
-        print('Você deve digitar o ano de seu nascimento: [XXXX]')
-        ano_nasc = int(input('INFORME O ANO DE SEU NASCIMENTO: '))
-        sexo = str(input('INFORME O SEU SEXO [M] ou [F]: '))
-    op = str(input(
-        '========== OPÇÕES ==========\nDIGITE [ 1 ] PARA PROSSEGUIR\nDIGITE [ 0 ] PARA SAIR\nESCOLHA SUA OPÇÃO: '))
+        while sexo != sexo in 'Mm' or 'Ff':
+            print('Você deve digitar "M" ou "F"')
+            sexo = str(input('INFORME O SEU SEXO [M] ou [F]: '))
+
 print('OPÇÃO REGISTRADA COM SUCESSO')
 
-    #break
