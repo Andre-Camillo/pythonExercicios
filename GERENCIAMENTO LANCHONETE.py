1cd741c5a0313446cf9521778e2ff6a85d251703
ANDRE DOUGLAS CAMILLO <127610857+Andre-Camillo@users.noreply.github.com>
2023-04-24 00:08:42 -0300

    Update GERENCIAMENTO LANCHONETE.py

--- GERENCIAMENTO LANCHONETE.py
+++ GERENCIAMENTO LANCHONETE.py
@@ -2,7 +2,8 @@ from emoji import emojize
 from time import sleep
 #CÓD EMOJIS UTILIZADOS
 #bauru :sandwich: - lanche :hamburger: - copo refri :cup_with_straw: - dinheiro :heavy_dollar_sign:
-colors = {'clear': '\033[m', 'red': '\033[31m', 'green': '\033[32m', 'yellow': '\033[33m', 'purple': '\033[35m', 'cyan': '\033[36m'}
+colors = {'clear': '\033[m', 'red': '\033[31m', 'green': '\033[32m', 'yellow': '\033[33m',
+          'purple': '\033[35m', 'cyan': '\033[36m', 'bold': '\033[1m'}
 quantxTudo = quantxEgg = quantxSalada = quantBauru = quantCoca = quantGuarana = quantFanta = quantTubaina = 0
 contxTudo = contxEgg = contxSalada = contBauru = contCoca = contGuarana = contFanta = contTubaina = 0
 ttxTudo = ttxEgg = ttxSalada = ttBauru = ttCoca = ttGuarana = ttFanta = ttTubaina = 0
@@ -19,31 +20,33 @@ vlFanta = 6
 vlTubaina = 5
 
 print(f"""{'=' * 44}
-{'=' * 14} ANDRE'S LANCHES {'=' * 13}
+{'=' * 14} {colors['bold']}ANDRE'S LANCHES{colors['clear']} {'=' * 13}
 {'=' * 44}""")
 
 while opc != 0:
     opc = int(input(f"""
 {'*' * 44}
-{'*' * 12}   MENU PRINCIPAL   {'*' * 12}
+{'*' * 12}   {colors['bold']}MENU PRINCIPAL{colors['clear']}   {'*' * 12}
 {'*' * 44}
 {'*' * 12}   1) - LANCHES   {'*' * 14}
 {'*' * 12}   2) - BEBIDAS   {'*' * 14}
 {'*' * 12}   3) - CONTA   {'*' * 16}
 {'*' * 12}   0) - SAIR   {'*' * 17}
 {'*' * 44}
+{'*' * 44}
 
 ESCOLHA SUA OPÇÃO: """))
-
+    print('')
     if opc == 1:
         menuLanche = int(input(f'''{emojize(':hamburger:')*18}
-{emojize(':hamburger:')*5}   MENU LANCHES  {emojize(':hamburger:')*6}
+{emojize(':hamburger:')*5}   {colors['bold']}MENU LANCHES{colors['clear']}  {emojize(':hamburger:')*6}
 {emojize(':hamburger:')*18}
 {emojize(f':hamburger:')*5}   {colors['green']}1) - X-TUDO{colors['clear']}   {emojize(':hamburger:')*6}
 {emojize(f':hamburger:')*5}   {colors['yellow']}2) - X-EGG{colors['clear']}    {emojize(':hamburger:')*6}
 {emojize(f':hamburger:')*5}   {colors['cyan']}3) - X-SALADA{colors['clear']} {emojize(':hamburger:')*6}
 {emojize(f':hamburger:')*5}   {colors['purple']}4) - BAURÚ{colors['clear']}    {emojize(':hamburger:')*6}
 {emojize(f':hamburger:')*5}   {colors['red']}0) - VOLTAR{colors['clear']}   {emojize(':hamburger:')*6}
+{emojize(f':hamburger:')*18}
 {emojize(f':hamburger:')*18}
 
 ESCOLHA SEU LANCHE: '''))
@@ -69,8 +72,8 @@ ESCOLHA SEU LANCHE: '''))
             contBauru += quantBauru
 
     if opc == 2:
-        menuBebidas = int(input(f'''{emojize(':cup_with_straw:')*21}
-{emojize(':cup_with_straw:')*6}   MENU BEBIDAS  {emojize(':cup_with_straw:')*7}
+        menuBebidas = int(input(f"""{emojize(':cup_with_straw:')*21}
+{emojize(':cup_with_straw:')*6}   {colors['bold']}MENU BEBIDAS{colors['clear']}  {emojize(':cup_with_straw:')*7}
 {emojize(':cup_with_straw:')*21}
 {emojize(f':cup_with_straw:')*6}   {colors['green']}1) - COCA-COLA{colors['clear']}  {emojize(':cup_with_straw:')*6}
 {emojize(f':cup_with_straw:')*6}   {colors['cyan']}2) - GUARANÁ{colors['clear']}    {emojize(':cup_with_straw:')*6}
@@ -79,7 +82,7 @@ ESCOLHA SEU LANCHE: '''))
 {emojize(':cup_with_straw:')*6}   {colors['red']}0) - VOLTAR{colors['clear']}     {emojize(':cup_with_straw:')*6}
 {emojize(':cup_with_straw:')*21}
 
-ESCOLHA SUA BEBIDA: '''))
+ESCOLHA SUA BEBIDA: """))
         if menuBebidas == 1:
             print(f"ESCOLHEU {colors['green']}COCA-COLA{colors['clear']} {emojize(':cup_with_straw:')}")
             print('')
@@ -104,7 +107,7 @@ ESCOLHA SUA BEBIDA: '''))
     if quantxTudo or quantxEgg or quantxSalada or quantBauru or quantCoca or quantGuarana or quantFanta or quantTubaina > 0:
         print('')
         print('=' * 44)
-        parcial = print(f"{' ' * 19}PEDIDO")
+        print(f"{' ' * 19}PEDIDO")
         print('-' * 44)
         if quantxTudo > 0:
             print(f"{contxTudo} - {colors['green']}X-TUDO{colors['clear']}      UNI R$ {vlxTudo:.2f} - TOT R$ {contxTudo * vlxTudo:.2f}")
@@ -132,18 +135,14 @@ ESCOLHA SUA BEBIDA: '''))
             ttTubaina = contTubaina * vlTubaina
     if opc == 3:
         print('=' * 44)
-        print(f'''VALOR A PAGAR: {' ' * 20}R$ {ttxTudo + ttxEgg + ttxSalada + ttBauru + ttCoca + ttGuarana + ttFanta + ttTubaina:.2f}''')
+        vlFinal = print(f'''VALOR A PAGAR: {' ' * 20}R$ {ttxTudo + ttxEgg + ttxSalada + ttBauru + ttCoca + ttGuarana + ttFanta + ttTubaina:.2f}''')
         print('=' * 44)
         print('')
         fin = str(input('''SEU PEDIDO ESTÁ COMPLETO?
     S - PARA SIM
     N - PARA NÃO: '''))
+
         if fin[0] in 'sS':
-            print('')
-            pgto = str(input('''ESCOLHA A FORMA DE PAGAMENTO:
-    1) DINHEIRO
-    2) CARTÃO
-    3) VOUCHER '''))
             print(f"{colors['green']}PROCESSANDO{colors['clear']}", end='')
             sleep(.5)
             print('.', end='')
@@ -157,14 +156,21 @@ ESCOLHA SUA BEBIDA: '''))
             print('.')
             sleep(.5)
             print('')
-            print('IMPRIMINDO', end='')
+            #print('')
+            pgto = str(input('''ESCOLHA A FORMA DE PAGAMENTO:
+    1) DINHEIRO
+    2) CARTÃO
+    3) VOUCHER: '''))
+            print('')
+            print("IMPRIMINDO", end='')
             sleep(.5)
             print('.', end='')
             sleep(.5)
             print('.', end='')
             sleep(.5)
-            print('.', end='')
+            print('.')
             sleep(.5)
+            print("PEDIDO FINALIZADO")
             break
         elif fin[0] in 'nN':
             opc = opc
