cb6e58f1669b5744ad9466ee95a3505ece0952f1
ANDRE DOUGLAS CAMILLO <127610857+Andre-Camillo@users.noreply.github.com>
2023-05-09 13:57:22 -0300

    .

--- TESTE.py
+++ TESTE.py
@@ -1,8 +1,10 @@
-num = ('DIGITE O 1º VALOR: ', 'DIGITE O 2º VALOR: ', 'DIGITE O 3º VALOR: ', 'DIGITE O 4º VALOR: ')
-n1 = str(input(num[0]))
-n2 = str(input(num[1]))
-n3 = str(input(num[2]))
-n4 = str(input(num[3]))
-num = n1, n2, n3, n4
-print(num)
-print(f'{num.count(9)}')
+lista = ('lapis', 1.75, 'borracha', 2, 'caderno', 15.90, 'estojo', 25, 'tranferidor', 4.20, 'compasso', 9.99,
+         'mochila', 120.32, 'canetas', 22.30, 'livro', 34.90)
+print('-' * 40)
+print(f'{"LISTAGEM DE PREÇOS" :^40}')
+print('-' * 40)
+for c in range(len(lista)):
+    if c % 2 == 0:
+        print(f' {lista[c]:.<27}'.upper(), end='')
+    else:
+        print(f' R$ {lista[c]:>7.2f}'.upper())
