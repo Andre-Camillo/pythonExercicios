a69f683ba269c5a4c057147a81970452b4a68db1
ANDRE DOUGLAS CAMILLO <127610857+Andre-Camillo@users.noreply.github.com>
2023-05-11 22:46:12 -0300

    .

--- TESTE.py
+++ TESTE.py
@@ -1,16 +1,16 @@
-#val = list()
-#val.append(1)
-#val.append(2)
-#val.append(3)
-#for pos, c in enumerate(val):
-#    print(f'NA POSIÇÃO {pos + 1} ESTÁ O VALOR: {c}')
+val = list()
+val.append(1)
+val.append(2)
+val.append(3)
+for pos, c in enumerate(val):
+    print(f'NA POSIÇÃO {pos + 1} ESTÁ O VALOR: {c}')
 
-#for c in range(0, 3):
-#    val.append(int(input('INSIRA O VALOR: ')))
+for c in range(0, 3):
+    val.append(int(input('INSIRA O VALOR: ')))
 
-#for pos, l in enumerate(val):
-#    print(f'NA POSIÇÃO {pos + 1} ESTÁ O VALOR: {l}')
-    #print(f'{l}', end=' ')
+for pos, l in enumerate(val):
+    print(f'NA POSIÇÃO {pos + 1} ESTÁ O VALOR: {l}')
+    print(f'{l}', end=' ')
 print('')
 a = [2, 3, 4, 7]
 b = a
