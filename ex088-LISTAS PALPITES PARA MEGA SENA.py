6dd92f5a8337442604cc3601d6783b4fbab06992
ANDRE DOUGLAS CAMILLO <127610857+Andre-Camillo@users.noreply.github.com>
2023-06-16 17:46:32 -0300

    .

--- ex088-LISTAS PALPITES PARA MEGA SENA.py
+++ ex088-LISTAS PALPITES PARA MEGA SENA.py
@@ -1,8 +1,23 @@
 from random import randint
-qt_Jgs = int(input('INFORME A QUANTIDADE DE JOGOS: '))
-for q in range(1, qt_Jgs + 1):
-    print(f'\nJOGO Nº {q}', end=' ')
-    for c in range(1, 7):
-        nums = randint(1, 60)
-        print(f'[{nums}]', end=' ')
-
+from time import sleep
+lista = []
+jogos = list()
+quant = int(input('INFORME A QUANTIDADE DO JOGOS: '))
+tot = 1
+while tot <= quant:
+    cont = 0
+    while True:
+        num = randint(0, 61)
+        if num not in lista:
+            lista.append(num)
+            cont += 1
+        if cont >= 6:
+            break
+    lista.sort()
+    jogos.append(lista[:])
+    lista.clear()
+    tot += 1
+print('-=' * 3, f'SORTEANDO {quant} JOGOS', '-=' * 3)
+for i, l in enumerate(jogos):
+    print(f'JOGOS {i + 1}: {l}')
+    sleep(0.5)
